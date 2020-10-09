9ca7323ec5cc360fc910a710d88192db3471efde
Nilanthy Balendra <nilanthy.balendra@progenity.com>
2020-10-09 08:54:35 -0400

    aggregate tsv script improved

--- cat_poly_tsv.py
+++ cat_poly_tsv.py
@@ -1,38 +1,69 @@
 #this script aggregates together run.tsv and sample.tsv files from Polyphemus. Pandas is used as columns are not
 #always in the same order
+
+import argparse
+import os
+import pandas as pd
+
 from pathlib import Path
 
-import pandas as pd
-import os
 
-main_path = Path('/mnt/prod_bfx_analysis_ro/analysis_data/progenity_workflow_polyphemus/flowcell_result')
-#main_path = Path('/mnt/prod_ro/sequencing/ops/production/nipt9002/results')
+def aggregate_tsv(suffix, out_dir):
 
-run_filename = 'poly_run_0814.tsv'
-sample_filename = 'poly_sample_0814.tsv'
+    main_path = Path('/mnt/prod_bfx_analysis_ro/analysis_data/progenity_workflow_polyphemus/flowcell_result')
+    #main_path = Path('/mnt/prod_ro/sequencing/ops/production/nipt9002/results')  # old prod location, pre 2.0
 
-fc = os.listdir(main_path)
+    run_filename = out_dir / f'poly_run_{suffix}.tsv'
+    sample_filename = out_dir / f'poly_sample_{suffix}.tsv'
 
-all_run = []
-all_sample = []
-for f in fc:
-    print(f)
+    fc = os.listdir(main_path)
 
-    run_path = main_path / f / 'tsv' / 'run.tsv'
-    sample_path = main_path / f / 'tsv' / 'samples.tsv'
-    #run_path = main_path / f / 'Bioinformatics_Project_Poly_9002' /'tsv' / 'Run_Project_Poly_9002_run.tsv'
+    all_run = []
+    all_sample = []
 
-    if os.path.isfile(run_path):
-        run_tsv = pd.read_csv(run_path, sep='\t', header = 0)
-        all_run.append(run_tsv)
+    print('Reading in files...')
 
-    if os.path.isfile(sample_path):
-        sample_tsv = pd.read_csv(sample_path, sep='\t', header=0)
-        all_sample.append(sample_tsv)
+    for f in fc:
+
+        run_path = main_path / f / 'tsv' / 'run.tsv'
+        sample_path = main_path / f / 'tsv' / 'samples.tsv'
+        #run_path = main_path / f / 'Bioinformatics_Project_Poly_9002' /'tsv' / 'Run_Project_Poly_9002_run.tsv' # old prod location, pre 2.0
+
+        if os.path.isfile(run_path):
+            run_tsv = pd.read_csv(run_path, sep='\t', header = 0)
+            all_run.append(run_tsv)
+
+        if os.path.isfile(sample_path):
+            sample_tsv = pd.read_csv(sample_path, sep='\t', header=0)
+            all_sample.append(sample_tsv)
+
+    print('Writing out files...')
+    poly_run = pd.concat(all_run, axis=0, sort=False, join='inner')
+    poly_run.to_csv(run_filename, sep='\t', index=None)
+
+    poly_run = pd.concat(all_sample, axis=0, sort=False, join='inner')
+    poly_run.to_csv(sample_filename, sep='\t', index=None)
+
+    print('Complete!')
+    print(run_filename)
+    print(sample_filename)
 
 
-poly_run = pd.concat(all_run, axis=0, sort=False, join='inner')
-poly_run.to_csv(run_filename, sep='\t', index=None)
+def arg_parser():
+    parser = argparse.ArgumentParser()
 
-poly_run = pd.concat(all_sample, axis=0, sort=False, join='inner')
-poly_run.to_csv(sample_filename, sep='\t', index=None)
+    parser.add_argument('suffix', type=str, help='output file suffix (ex. date like 1009)')
+    parser.add_argument('output_path', type=Path, help='output path')
+
+    return parser
+
+
+def cli():
+    parser = arg_parser()
+    args = parser.parse_args()
+
+    aggregate_tsv(args.suffix, args.output_path)
+
+
+if __name__ == "__main__":
+    cli()
